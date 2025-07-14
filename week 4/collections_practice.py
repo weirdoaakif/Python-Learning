cfc6d8dbfc4730a6513a29dce694ce8fdd2e6a01
weirdoaakif <aakifraj17@gmail.com>
2025-07-14 11:16:49 +0530

    Organize initial Python practice files

---
+++ week 4/collections_practice.py
@@ -0,0 +1,27 @@
+#1. List Example
+fruits = ["aaple", "banana", "cherry"]
+fruits.append("orange")
+print("Fruits:", fruits)
+
+#2. Tuple Example
+coordinates = (10,20)
+print("Coordinates:", coordinates)
+
+#3. Dictionary Example
+student = {
+    "name": "Aakif",
+    "age": 18,
+    "course": "Python"
+}
+print("Student:", student)
+
+#4. Set Example
+unique_numbers = {1,2,3,2,1}
+print("Unique Numbers:", unique_numbers)
+
+#5. List of Dictionaries
+students = [
+    {"name": "Ali", "age": 18},
+    {"name": "Sara", "age": 19}
+]
+print("Students:", students)
